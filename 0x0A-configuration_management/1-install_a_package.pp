cc592ebd99731a37bf98a69918dd800ca1ae655b
jeffmakuto <hjeffmakuto@proton.me>
2023-11-24 04:18:35 -0800

    Configuration Management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,7 @@
-# install flask from pip3
-package { 'flask' :
-    ensure   => '2.1.0',
-    provider => 'pip3',
-    }
+# 1-install_a_package.pp
+
+# Install flask using pip3 with a specific version
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
